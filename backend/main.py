a37023b23381a6c265d19d32ecd0b1cf77aff4d4
DicksonLegend <dicksyn2006@gmail.com>
2025-08-09 16:21:52 +0530

    Updated main

--- backend/main.py
+++ backend/main.py
@@ -15,8 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Include /prompt route
-app.include_router(prompts.router)
 # Include /generate-prompt route
 app.include_router(generator_router)
 
